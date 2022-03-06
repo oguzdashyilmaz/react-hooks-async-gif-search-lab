e52416979843c23f7fe9fe13d96586c84279e919
Oguz <yilmazoguz@outlook.com>
2022-03-06 16:38:04 +0300

    Import GifListContainer into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import NavBar from "./NavBar";
+import GifListContainer from "./GifListContainer";
 
 function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer/>
     </div>
   );
 }
