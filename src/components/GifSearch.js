f94ec4ab9455e866a14827c74388aaffa6891139
Oguz <yilmazoguz@outlook.com>
2022-03-06 19:15:08 +0300

    Create GifSearch.js

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -4,11 +4,25 @@ import {useState} from "react";
 function GifSearch({onSubmitSearch}) {
   
     const [search, setSearch] = useState("");
-    
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        onSubmitSearch(search);
+    }
+
+    function handleChange(event) {
+        setSearch(event.target.value);
+    }
 
     return (
     <div>
-
+        <form action="" onSubmit={handleSubmit}>
+            <div className='form-container'>
+                <label htmlFor="search">Search for gifs: </label>
+                <input type="text" id='search' className='form-elements' value={search} onChange={handleChange} />
+            </div>
+            <button type='submit' className='btn btn-success'>Find gifs</button>
+        </form>
     </div>
   )
 }
