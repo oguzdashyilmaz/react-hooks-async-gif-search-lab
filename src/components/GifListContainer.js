4233449eb18ffb7dc041aaf7a6ed77ae95cd3cc5
Oguz <yilmazoguz@outlook.com>
2022-03-06 17:01:26 +0300

    Set useEffect for GifListContainer.js

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react/cjs/react.development';
 
 function GifListContainer() {
 
     const [gifs, setGifs] = useState([]);
     const [search, setSearch] = useState("");
 
-  return (
-    <div>GifListContainer</div>
-  )
+    useEffect(() => {
+        
+        const fetchGifs = async () => {
+            const res = await fetch("https://api.giphy.com/v1/gifs/search?q=" + search + "api_key=dc6zaTOxFJmzC&rating=g");
+        };
+
+        const json = await res.json();
+
+        const data = json.data;
+
+        const gifsData = data.map(gif=>{
+            const {id, title, images} = gif;
+            return {id: id, title: title, image: images.original.url}
+        })
+        setGifs(gifsData);
+    fetchGifs()}, [setSearch]);
+
+    return (
+        <div className='gifListContainer'>
+            {gifs && <GifList gifs={gifs} />}
+            <GifSearch onSearch={handeSearch} />
+        </div>
+    )
 }
 
 export default GifListContainer
