41ce2400db20c284ae944848cc89638667b40f75
Oguz <yilmazoguz@outlook.com>
2022-03-06 16:37:08 +0300

    Create a complete component of GifListContainer.jsx

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import NavBar from "./NavBar";
 
-// the App component should render out the GifListContainer component
-
 function App() {
   return (
     <div>
---
+++ src/components/GifListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GifListContainer() {
+  return (
+    <div>GifListContainer</div>
+  )
+}
+
+export default GifListContainer
