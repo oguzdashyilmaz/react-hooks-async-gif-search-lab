b39b2b33582338b9472e662ba4daeeb55d2654ca
Oguz <yilmazoguz@outlook.com>
2022-03-06 16:42:24 +0300

    Set useStates within GifListContainer.js

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function GifListContainer() {
+
+    const [gifs, setGifs] = useState([]);
+    const [search, setSearch] = useState("");
+
   return (
     <div>GifListContainer</div>
   )
